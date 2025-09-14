efb37182e960c70883269c5bd1534beb755ee179
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-09-14 17:35:03 +0200

    Cosmetic changes
    - Add a spinner to posts retrieval
    - Improve loading indicators
    - Add Top authors

--- internal/spinner/spinner.go
+++ internal/spinner/spinner.go
@@ -7,26 +7,31 @@ import (
 )
 
 type Spinner struct {
-	cnt    int
-	out    io.Writer
-	format string
-	done   chan struct{}
-	tick   chan struct{}
+	out      io.Writer
+	done     chan struct{}
+	tick     chan string
+	maxChars int
 }
 
-func New(out io.Writer, format string) *Spinner {
+func New(out io.Writer) *Spinner {
 	s := &Spinner{
-		out:    out,
-		format: format,
-		done:   make(chan struct{}),
-		tick:   make(chan struct{}),
+		out:  out,
+		done: make(chan struct{}),
+		tick: make(chan string),
 	}
 	return s
 }
 
-func (s *Spinner) Start(ctx context.Context) {
+func (s *Spinner) print(str string) {
+	s.maxChars = max(s.maxChars, len(str))
+	fmt.Fprintf(s.out, "%*s", s.maxChars, str)
+}
+
+func (s *Spinner) Start(ctx context.Context, str string) {
+	s.print(str)
 	spinningCharacters := []rune("⣾⣽⣻⢿⡿⣟⣯⣷")
 	go func() {
+		var pos int
 		for {
 			select {
 			case <-s.done:
@@ -34,22 +39,21 @@ func (s *Spinner) Start(ctx context.Context) {
 
 			case <-ctx.Done():
 				return
-			case <-s.tick:
-				s.cnt++
-				pos := s.cnt % len(spinningCharacters)
-				fmt.Fprint(s.out, "\r"+string(spinningCharacters[pos]))
-				fmt.Fprintf(s.out, " "+s.format+"…", s.cnt)
+			case str := <-s.tick:
+				pos = (pos + 1) % len(spinningCharacters)
+				animated := "\r" + string(spinningCharacters[pos]) + " " + str
+				s.print(animated)
 			}
 		}
 	}()
 }
 
-func (s *Spinner) Inc() {
-	s.tick <- struct{}{}
+func (s *Spinner) Progress(format string, args ...any) {
+	s.tick <- fmt.Sprintf(format, args...)
 }
 
-func (s *Spinner) Done() {
-	fmt.Fprintf(s.out, "\r✔️ "+s.format+"       \n\n", s.cnt)
+func (s *Spinner) Done(format string, args ...any) {
+	s.print("\r" + fmt.Sprintf(format, args...) + "\n")
 	close(s.done)
 	close(s.tick)
 }
